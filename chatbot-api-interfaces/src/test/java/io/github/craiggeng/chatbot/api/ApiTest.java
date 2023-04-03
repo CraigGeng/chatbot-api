a1942591845e306818a15a601368dcb89c669e70
craiggeng <841082723@qq.com>
2023-04-03 16:11:41 +0800

    知识星球爬虫

--- chatbot-api-interfaces/src/test/java/io/github/craiggeng/chatbot/api/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/io/github/craiggeng/chatbot/api/ApiTest.java
@@ -1,4 +1,26 @@
 package io.github.craiggeng.chatbot.api;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+
 public class ApiTest {
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet("");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else{
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
 }
